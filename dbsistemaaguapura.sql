4e792e134a51aa341ae9911327dd0ecc2bec7408
limberrodasruiz <44888585+limberrodasruiz@users.noreply.github.com>
2019-07-15 14:58:34 -0400

    Add files via upload

---
+++ dbsistemaaguapura.sql
@@ -0,0 +1,554 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-07-2019 a las 20:57:17
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dbsistemaaguapura`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`, `condicion`, `created_at`, `updated_at`) VALUES
+(1, 'Embutidos', 'todo tipo de carnes rojas y mas', 0, NULL, '2019-06-22 06:44:37'),
+(2, 'Cereales', 'Todos los cereales', 0, NULL, '2019-06-23 01:03:35'),
+(3, 'Oficina', 'Todos los equipos para la oficina', 1, '2019-06-18 18:08:41', '2019-06-18 18:08:41'),
+(4, 'Hogar', 'Todo para el Hogar y mucho mas', 1, '2019-06-18 19:44:26', '2019-06-18 19:44:26'),
+(5, 'Empresas', 'Equipos solo para empresas', 1, '2019-06-18 20:41:06', '2019-06-18 23:53:24'),
+(6, 'Mesa', 'Todo para la mesa de su casa', 1, '2019-06-19 01:40:36', '2019-06-19 01:40:36'),
+(7, 'Jarras', NULL, 1, '2019-06-19 01:41:16', '2019-06-19 01:41:16'),
+(8, 'Botellas', 'Gaseosas', 1, '2019-06-19 01:41:25', '2019-07-10 03:18:43'),
+(9, 'Luces', 'Todo luces', 1, '2019-06-19 04:07:15', '2019-06-19 04:07:15'),
+(10, 'Cursos', 'Matematicas aplicadas', 1, '2019-06-19 04:08:39', '2019-06-23 18:49:42');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ingresos`
+--
+
+CREATE TABLE `detalle_ingresos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idingreso` int(10) UNSIGNED NOT NULL,
+  `idproducto` int(10) UNSIGNED NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_ingresos`
+--
+
+INSERT INTO `detalle_ingresos` (`id`, `idingreso`, `idproducto`, `cantidad`, `precio`) VALUES
+(23, 18, 6, 50, '2000.00'),
+(24, 18, 5, 20, '3.00'),
+(25, 19, 6, 50, '2000.00'),
+(26, 19, 5, 30, '3.00');
+
+--
+-- Disparadores `detalle_ingresos`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_updStockIngreso` AFTER INSERT ON `detalle_ingresos` FOR EACH ROW BEGIN
+ UPDATE productos SET stock = stock + NEW.cantidad 
+ WHERE productos.id = NEW.idproducto;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ventas`
+--
+
+CREATE TABLE `detalle_ventas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idventa` int(10) UNSIGNED NOT NULL,
+  `idproducto` int(10) UNSIGNED NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `descuento` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_ventas`
+--
+
+INSERT INTO `detalle_ventas` (`id`, `idventa`, `idproducto`, `cantidad`, `precio`, `descuento`) VALUES
+(1, 1, 1, 25, '4.00', '0.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingresos`
+--
+
+CREATE TABLE `ingresos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idproveedor` int(10) UNSIGNED NOT NULL,
+  `idusuario` int(10) UNSIGNED NOT NULL,
+  `tipo_comprobante` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `serie_comprobante` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `num_comprobante` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total` decimal(11,2) NOT NULL,
+  `estado` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ingresos`
+--
+
+INSERT INTO `ingresos` (`id`, `idproveedor`, `idusuario`, `tipo_comprobante`, `serie_comprobante`, `num_comprobante`, `fecha_hora`, `impuesto`, `total`, `estado`, `created_at`, `updated_at`) VALUES
+(18, 3, 1, 'BOLETA', '001', '0001', '2019-07-15 00:00:00', '0.18', '100060.00', 'Anulado', '2019-07-15 18:25:32', '2019-07-15 18:40:06'),
+(19, 3, 1, 'BOLETA', '002', '0002', '2019-07-15 00:00:00', '0.18', '100090.00', 'Anulado', '2019-07-15 18:53:40', '2019-07-15 18:54:20');
+
+--
+-- Disparadores `ingresos`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_updStockIngresoAnular` AFTER UPDATE ON `ingresos` FOR EACH ROW BEGIN
+  UPDATE productos a
+    JOIN detalle_ingresos di
+      ON di.idproducto = a.id
+     AND di.idingreso = new.id
+     set a.stock = a.stock - di.cantidad;
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_06_11_163935_create_categorias_table', 1),
+(4, '2019_06_19_001309_create_productos_table', 2),
+(5, '2019_06_23_145527_create_personas_table', 3),
+(7, '2019_06_23_222837_create_proveedores_table', 5),
+(9, '2019_06_23_235600_create_roles_table', 6),
+(10, '2019_06_23_235757_create_users_table', 7),
+(11, '2019_07_14_032246_create_ingresos_table', 8),
+(12, '2019_07_14_032709_create_detalle_ingresos_table', 8),
+(13, '2019_07_15_163905_create_ventas_table', 9),
+(14, '2019_07_15_163958_create_detalle_ventas_table', 9);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo_documento` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `num_documento` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `direccion` varchar(70) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`, `created_at`, `updated_at`) VALUES
+(1, 'Limber rodas', 'DNI', '8182104', 'B/Pantanal', '76071097', 'rrr_limber@hotmail.com', NULL, '2019-06-24 03:08:02'),
+(2, 'Ronal Rodas Ruiz', 'DNI', '4896555', 'B/ Pantanal', '65001847', 'rrr_ronal@hotmail.com', '2019-06-24 01:15:30', '2019-06-24 01:15:52'),
+(3, 'Agua purita sa', 'RUC', '65465464', 'av america', '33232323', 'agua@gmail.com', '2019-06-24 03:12:10', '2019-06-24 03:12:51'),
+(25, 'Edwin Ruiz', 'DNI', '989898', 'av america', '78795456', 'limber_devil@hotmail.com', '2019-06-24 06:00:05', '2019-06-24 06:00:42'),
+(26, 'Ronal rodas', 'DNI', '9898995', 'av. miguel de cervantes', '65008747', 'rrr_ronal@hotmail.com', '2019-07-14 07:03:05', '2019-07-14 07:03:05');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idcategoria` int(10) UNSIGNED NOT NULL,
+  `codigo` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descripcion` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `idcategoria`, `codigo`, `nombre`, `precio_venta`, `stock`, `descripcion`, `condicion`, `created_at`, `updated_at`) VALUES
+(1, 4, '01113554', 'Jarras para el hogar con filtro', '10.50', 100, NULL, 1, NULL, NULL),
+(2, 2, '3265656', 'Chocomic', '12.00', 50, 'cereal sabor a chocolate', 1, NULL, '2019-06-23 07:48:19'),
+(3, 4, '454564646', 'coca', '4.00', 100, NULL, 1, '2019-06-23 07:48:13', '2019-06-23 18:20:35'),
+(4, 8, '131312', 'Jarra con filtro', '180.00', 200, NULL, 1, '2019-06-23 07:49:03', '2019-06-23 18:23:11'),
+(5, 9, '1231321', 'Focos', '5.00', 70, NULL, 1, '2019-06-23 17:08:20', '2019-06-23 18:07:38'),
+(6, 8, '11314545', 'Dispenser', '2250.00', 150, NULL, 1, '2019-06-23 17:19:24', '2019-06-23 18:50:12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `contacto` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono_contacto` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id`, `contacto`, `telefono_contacto`) VALUES
+(1, 'rodas ruiz', '78454569'),
+(3, 'Purifica', '77989894'),
+(25, '76071097', '78454569');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `nombre`, `descripcion`, `condicion`) VALUES
+(1, 'Administrador', 'Administradores de área', 1),
+(2, 'Vendedor', 'Vendedor área venta', 1),
+(3, 'Almacenero', 'Almacenero área compras', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `usuario` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT '1',
+  `idrol` int(10) UNSIGNED NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `usuario`, `password`, `condicion`, `idrol`, `remember_token`) VALUES
+(1, 'Admin', '$2y$10$CD4HPD8mm4XXL/qZB.N5ReQmWMd8MiQMkVxn/UuEwx9zA7nmwbaia', 1, 1, '4ptTZPwZpnpyYyY7pcu5DuJBnZyrkNYmlCbvjBI2iFEfYzYxe5Es3JsCd4Ud'),
+(26, 'ronal', '$2y$10$hV5i58hh0Zld/wsBsQy9GODT4zLaIYjnQbfnxPiFKJCweHuMp0PRu', 1, 3, 'bc2IYOlvG3poylK5qcXMqvyKbhyeXVqdFb5YQMINa1l1b9nLKEXVBto1cl0x'),
+(25, 'User', '$2y$10$C9EvR8TaAIL6pEKVdvKJLeoVdTRgn/b4frG2bv1G4Lhn5SuLXPdx6', 1, 2, 'Gyjgn86CZRsSY5hv4KfuAJ1nrnKbAgPGEMgcuQrnxaAoijl4v6ErTR1N2Zqx');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idcliente` int(10) UNSIGNED NOT NULL,
+  `idusuario` int(10) UNSIGNED NOT NULL,
+  `tipo_comprobante` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `serie_comprobante` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `num_comprobante` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total` decimal(11,2) NOT NULL,
+  `estado` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id`, `idcliente`, `idusuario`, `tipo_comprobante`, `serie_comprobante`, `num_comprobante`, `fecha_hora`, `impuesto`, `total`, `estado`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'Boleta', '001', '0001', '2019-07-15 15:00:00', '0.18', '100.00', 'Registrado', NULL, NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `detalle_ingresos`
+--
+ALTER TABLE `detalle_ingresos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `detalle_ingresos_idingreso_foreign` (`idingreso`),
+  ADD KEY `detalle_ingresos_idproducto_foreign` (`idproducto`);
+
+--
+-- Indices de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `detalle_ventas_idventa_foreign` (`idventa`),
+  ADD KEY `detalle_ventas_idproducto_foreign` (`idproducto`);
+
+--
+-- Indices de la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ingresos_idproveedor_foreign` (`idproveedor`),
+  ADD KEY `ingresos_idusuario_foreign` (`idusuario`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personas_nombre_unique` (`nombre`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `productos_nombre_unique` (`nombre`),
+  ADD KEY `productos_idcategoria_foreign` (`idcategoria`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD KEY `proveedores_id_foreign` (`id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roles_nombre_unique` (`nombre`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `users_usuario_unique` (`usuario`),
+  ADD KEY `users_id_foreign` (`id`),
+  ADD KEY `users_idrol_foreign` (`idrol`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ventas_idcliente_foreign` (`idcliente`),
+  ADD KEY `ventas_idusuario_foreign` (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ingresos`
+--
+ALTER TABLE `detalle_ingresos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_ingresos`
+--
+ALTER TABLE `detalle_ingresos`
+  ADD CONSTRAINT `detalle_ingresos_idingreso_foreign` FOREIGN KEY (`idingreso`) REFERENCES `ingresos` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detalle_ingresos_idproducto_foreign` FOREIGN KEY (`idproducto`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `detalle_ventas`
+--
+ALTER TABLE `detalle_ventas`
+  ADD CONSTRAINT `detalle_ventas_idproducto_foreign` FOREIGN KEY (`idproducto`) REFERENCES `productos` (`id`),
+  ADD CONSTRAINT `detalle_ventas_idventa_foreign` FOREIGN KEY (`idventa`) REFERENCES `ventas` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD CONSTRAINT `ingresos_idproveedor_foreign` FOREIGN KEY (`idproveedor`) REFERENCES `proveedores` (`id`),
+  ADD CONSTRAINT `ingresos_idusuario_foreign` FOREIGN KEY (`idusuario`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_idcategoria_foreign` FOREIGN KEY (`idcategoria`) REFERENCES `categorias` (`id`);
+
+--
+-- Filtros para la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD CONSTRAINT `proveedores_id_foreign` FOREIGN KEY (`id`) REFERENCES `personas` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_id_foreign` FOREIGN KEY (`id`) REFERENCES `personas` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `users_idrol_foreign` FOREIGN KEY (`idrol`) REFERENCES `roles` (`id`);
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_idcliente_foreign` FOREIGN KEY (`idcliente`) REFERENCES `personas` (`id`),
+  ADD CONSTRAINT `ventas_idusuario_foreign` FOREIGN KEY (`idusuario`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
